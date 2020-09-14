8051338613c5b6804f1b6d194b4dd42613e6040b
Wesley Wesleystella Stella <wesleystella@Wesleys-iMac.local>
2020-09-13 23:44:24 -0300

    Finished

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,11 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  const formatConfig = {
+    style: 'currency',
+    currency: 'BRL',
+    currencyDisplay: 'symbol',
+  };
+  const balanceValue = Intl.NumberFormat('pt-BR', formatConfig).format(value);
+  return balanceValue;
+};
 
 export default formatValue;
